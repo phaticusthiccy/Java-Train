42b826c0e38b21a21b7c23eb38237d62383fef07
Thiccy <birhic023@gmail.com>
2021-07-25 00:48:05 +0300

    Create basic_consoleout.java

---
+++ basic_consoleout.java
@@ -0,0 +1,18 @@
+/**
+* Phaticusthiccy Emacs-Train @2021 
+* All Right Reserved
+*/
+
+public class Phaticusthiccy {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("This is Thiccy. 17 Years Old AI Developer.");
+    
+  }
+  
+}
+
+/**
+* >> This is Thiccy. 17 Years Old AI Developer.
+*/
