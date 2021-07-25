efae5640056fef4a5634de02383348d0044b0a1a
Thiccy <birhic023@gmail.com>
2021-07-25 04:11:57 +0300

    Create find-ascii-value.java

---
+++ by-input/find-ascii-value.java
@@ -0,0 +1,18 @@
+/**
+* Phaticusthiccy Java-Train @2021 
+* All Right Reserved
+*/
+
+public class AsciiValue {
+
+    public static void main(String[] args) {
+     
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
