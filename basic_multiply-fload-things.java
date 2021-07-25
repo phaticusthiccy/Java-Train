c706aefddbd7ed0f860ceb2c18190b0c2a020e32
Thiccy <birhic023@gmail.com>
2021-07-25 04:09:15 +0300

    Create basic_multiply-fload-things.java

---
+++ basic_multiply-fload-things.java
@@ -0,0 +1,31 @@
+/**
+* Phaticusthiccy Java-Train @2021 
+* All Right Reserved
+*/
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+
+/** 
+Explanation of This Module
+*/
+
+/**
+* -- In the above program, we have two floating-point numbers 1.5f and 2.0f stored in variables first and second respectively.
+
+* -- Notice, we have used f after the numbers. This ensures the numbers are float, otherwise they will be assigned - type double.
+
+* -- first and second are then multiplied using the * operator and the result is stored in a new float variable product.
+
+* -- Finally, the result product is printed on the screen using println() function.
+*/
