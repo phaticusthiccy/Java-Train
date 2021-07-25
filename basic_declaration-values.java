9c8cc89ab854f7c117a2abe2d52e59cf32355b1b
Thiccy <birhic023@gmail.com>
2021-07-25 04:06:15 +0300

    Create basic_declaration-values.java

---
+++ basic_declaration-values.java
@@ -0,0 +1,26 @@
+/**
+* Phaticusthiccy Java-Train @2021 
+* All Right Reserved
+*/
+
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int first = 10;
+    int second = 20;
+    
+    System.out.println(first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
+
+/**
+* Enter two numbers
+* 10 20
+* The sum is: 30
+*/
